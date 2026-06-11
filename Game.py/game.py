8340a5fd58076e6f59d71a770647a5b790ecc230
monika <movi24ka@gmail.com>
2026-06-11 17:39:17 +0530

    Create game.py

---
+++ Game.py/game.py
@@ -0,0 +1,32 @@
+data = [
+    {
+        'name'         :"instgram",
+        'follower_count': 347,
+        'description'   : 'social media platform',
+        'country'       : 'United states',
+    },
+    {
+        'name': 'microsoft',
+        'follower_count' : 200,
+        'description': 'product based comp',
+        'country' : 'canada',
+    },
+    {
+        'name': 'National Geographic',
+        'follower_count': 135,
+        'description': 'Magazine',
+        'country': 'United States'
+    },
+    {
+        'name': 'ronaldinho',
+        'follower_count': 150,
+        'description': 'footballer',
+        'country': 'brasil'
+    },
+    {
+        'name': '9gag',
+        'follower_count': 52,
+        'description': 'social media platform',
+        'country': 'china',
+    }
+]
