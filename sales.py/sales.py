b2cef372bf9c5ba019e22e57725cfa2b6170cc34
monika <movi24ka@gmail.com>
2026-06-12 18:31:47 +0530

    Create sales.py

---
+++ sales.py/sales.py
@@ -0,0 +1,32 @@
+data = [
+    {
+        'name'         :"lambogine",
+        'sales_count': 347,
+        'description'   : 'car mode',
+        'country'       : 'Italy',
+    },
+    {
+        'name': 'starbucks',
+        'sales_count' : 200,
+        'description': 'product based comp',
+        'country' : 'canada',
+    },
+    {
+        'name': 'aws cloud',
+        'sales_count': 135,
+        'description': 'cloud platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'RWDY',
+        'sales_count': 150,
+        'description': 'clothing brand',
+        'country': 'India'
+    },
+    {
+        'name': '9gag',
+        'sales_count': 52,
+        'description': 'social media platform',
+        'country': 'china',
+    }
+]
